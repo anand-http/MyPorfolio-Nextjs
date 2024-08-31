8281c6163c4f363cca9f755cc8722f3d13fe6af8
Anand <rishusingh9369@gmail.com>
2024-08-31 14:28:32 +0530

    resume update

--- client_nextjs/app/container/Header/Header.jsx
+++ client_nextjs/app/container/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => (
 
         </div>
 
-        <a target='_blank' style={{ textDecoration: "none" }} rel="noreferrer" href="https://drive.google.com/file/d/1dNmNRxc5bPquT__CRXh1LQGYv6o2MJAO/view?usp=sharing">
+        <a target='_blank' style={{ textDecoration: "none" }} rel="noreferrer" href="https://drive.google.com/file/d/1V3_iifmEEyHQoqsuflo3Nt9QLbyKB15N/view?usp=sharing">
           <div className="tag-cmp app__flex">
             <Image width={"40"} src={cv} alt="Resume" />
             <p className="p-text resume">
